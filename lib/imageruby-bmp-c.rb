637add3a8c6d5b57e0e13aaea6bc85e597bf5565
Dario <darios3@gmail.com>
2011-05-08 13:38:04 -0300

    implemented encoder on pure ruby (forked from imageruby-bmp gem)

--- lib/imageruby-bmp-c.rb
+++ lib/imageruby-bmp-c.rb
@@ -34,12 +34,64 @@ module ImageRuby
   end
 
   class BmpCEncoder < ImageRuby::Decoder
+
     def encode(image, format, output)
-      if format != :bmp
+      unless format == :bmp
         raise UnableToEncodeException
       end
 
-      output << ImageRubyBmpC.encode_bitmap(image)
+      width = image.width
+      height = image.height
+
+      totalsize = 56 + width * height * 3
+
+      output << build_bmp_header(totalsize)
+      output << build_bmp_dib_header(image)
+
+      output << "\000\000" # for allignment
+
+      padding_size = ( 4 - (width * 3 % 4) ) % 4
+      padding =  "\000" * padding_size
+
+      # write pixel data
+      height.times do |y|
+        index = ((height-y-1)*width)*3
+        output << image.pixel_data[index..index+width*3-1]
+        output << padding
+      end
+    end
+
+  private
+    def build_bmp_dib_header(image)
+      data = String.new
+
+      data << [40].pack("L") #size of header
+      data << [image.width].pack("L")
+      data << [image.height].pack("L")
+      data << [1].pack("S") # nplanes
+      data << [0x18].pack("L")  # depth
+      data << [0].pack("S") # compress type
+      data << [image.width*image.height*3].pack("S") # raw bitmap data size
+
+      data << [0].pack("L") # hres
+      data << [0].pack("L") # vres
+      data << [0].pack("L") # ncolors
+      data << [0].pack("L") # nimpcolors
+
+      data
+    end
+    def build_bmp_header(totalsize)
+
+      data = String.new
+
+      data << "BM" # magic
+      data << [totalsize].pack("L") # totalsize
+      data << [0].pack("S") # creator1
+      data << [0].pack("S") # creator2
+      data << [54].pack("L") # offset
+
+      data
+
     end
   end
 
